b5227b81a9274a20d95e07fc40b9619afa8cafe4
Luis Otavio <luis.otavio2425@gmail.com>
2019-06-17 13:04:25 -0300

    Banco

---
+++ Assakabrasa V1.5.sql
@@ -0,0 +1,370 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 17-Jun-2019 às 18:03
+-- Versão do servidor: 10.1.40-MariaDB
+-- versão do PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `assakabrasa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_caixa`
+--
+
+CREATE TABLE `tb_caixa` (
+  `id` int(11) NOT NULL,
+  `saldo_inicial` decimal(10,2) NOT NULL,
+  `saldo_final` decimal(10,2) DEFAULT NULL,
+  `data_aberto` datetime NOT NULL,
+  `data_fechado` datetime DEFAULT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Extraindo dados da tabela `tb_caixa`
+--
+
+INSERT INTO `tb_caixa` (`id`, `saldo_inicial`, `saldo_final`, `data_aberto`, `data_fechado`, `id_usuario`, `status`) VALUES
+(49, '200.00', '200.00', '2019-06-14 12:57:29', '2019-06-14 12:57:32', 7, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_categorias`
+--
+
+CREATE TABLE `tb_categorias` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `data_registro` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tb_categorias`
+--
+
+INSERT INTO `tb_categorias` (`id`, `descricao`, `data_registro`) VALUES
+(1, 'Comidas234', '0000-00-00 00:00:00'),
+(2, 'Bebidas', '2019-06-06 10:48:45'),
+(3, 'Sobremesas', '0000-00-00 00:00:00'),
+(4, 'teste', '0000-00-00 00:00:00'),
+(8, '134', '0000-00-00 00:00:00'),
+(9, 'ert', '0000-00-00 00:00:00'),
+(10, '23', '2019-06-08 14:06:43');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_comandas`
+--
+
+CREATE TABLE `tb_comandas` (
+  `id` int(11) NOT NULL,
+  `nome_cliente` varchar(60) DEFAULT NULL,
+  `data_registro` datetime NOT NULL,
+  `data_pagamento` datetime DEFAULT NULL,
+  `tipo_pagamento` int(2) DEFAULT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_mesa` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Extraindo dados da tabela `tb_comandas`
+--
+
+INSERT INTO `tb_comandas` (`id`, `nome_cliente`, `data_registro`, `data_pagamento`, `tipo_pagamento`, `id_usuario`, `id_mesa`, `status`) VALUES
+(6, 'JoÃ£o', '2019-06-17 08:34:56', NULL, NULL, 7, 1, 0),
+(7, '', '2019-06-17 08:35:54', NULL, NULL, 7, 2, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_comandas_produtos`
+--
+
+CREATE TABLE `tb_comandas_produtos` (
+  `id` int(11) NOT NULL,
+  `quantidade` int(4) NOT NULL,
+  `id_comanda` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Extraindo dados da tabela `tb_comandas_produtos`
+--
+
+INSERT INTO `tb_comandas_produtos` (`id`, `quantidade`, `id_comanda`, `id_produto`) VALUES
+(7, 3, 6, 2),
+(8, 3, 6, 23),
+(9, 1, 6, 3),
+(10, 1, 6, 3),
+(11, 4, 6, 2),
+(12, 4, 6, 1),
+(13, 2, 6, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_mesas`
+--
+
+CREATE TABLE `tb_mesas` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `data_registro` datetime NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Extraindo dados da tabela `tb_mesas`
+--
+
+INSERT INTO `tb_mesas` (`id`, `descricao`, `data_registro`, `id_usuario`, `status`) VALUES
+(1, 'Mesa 1', '2019-06-12 10:06:37', 7, 1),
+(2, 'Mesa 2', '2019-06-12 10:06:41', 7, 1),
+(3, 'Mesa 3', '2019-06-12 10:06:46', 7, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_movimento_caixa`
+--
+
+CREATE TABLE `tb_movimento_caixa` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(60) DEFAULT NULL,
+  `tipo` int(1) NOT NULL,
+  `modo_pagamento` int(1) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `data_registro` datetime NOT NULL,
+  `id_caixa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_produtos`
+--
+
+CREATE TABLE `tb_produtos` (
+  `id` int(11) NOT NULL,
+  `cod` int(11) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `estoque` int(10) NOT NULL,
+  `data_registro` datetime NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Extraindo dados da tabela `tb_produtos`
+--
+
+INSERT INTO `tb_produtos` (`id`, `cod`, `descricao`, `valor`, `estoque`, `data_registro`, `id_categoria`, `id_usuario`) VALUES
+(1, 7893, 'Pizza', '50.00', 343, '0000-00-00 00:00:00', 2, 6),
+(2, 7898, 'Refrigerante', '50.00', 12, '0000-00-00 00:00:00', 1, 6),
+(3, 124143, 'Comidas', '40.00', 123, '2019-06-12 09:39:17', 1, 7),
+(23, 43534, 'Resto', '40.00', 3, '0000-00-00 00:00:00', 1, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_usuarios`
+--
+
+CREATE TABLE `tb_usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `sobrenome` varchar(60) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  `nivel` int(1) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+--
+-- Extraindo dados da tabela `tb_usuarios`
+--
+
+INSERT INTO `tb_usuarios` (`id`, `nome`, `sobrenome`, `email`, `senha`, `nivel`, `status`) VALUES
+(6, 'Luis3324', 'Otavio', 'luis.limalc@hotmail.com', '$2y$10$XfUcXf0Nmkzos6/LE741AuR.xvFKxCZF1qxSQ5lA1AgxCRwnpqlXu', 2, 1),
+(7, 'Luis', 'Otavio', 'admin@admin.com', '$2y$10$Tlwz9rkGCoUDvdcHyqUCcePzbYnU4MBSpoOCgYOYtUVMa1pdCCPYi', 2, 1),
+(9, 'Luis', 'Otavio', 'admin@admin.com', '$2y$10$bZPfC3fWhaljnGB1n2G2yOFuCeLRtS1c/j8.kTazlr/TC3FRk8Wfi', 2, 1),
+(10, 'Luis3324', 'Otavio', 'admi1234n@admin.com', '$2y$10$H3/EicJi5wt1BjKbBEE0Out31sxmKecHnISMNSDWTBWN.ESAjKH7W', 1, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_caixa`
+--
+ALTER TABLE `tb_caixa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tb_caixa_tb_usuarios_1` (`id_usuario`) USING BTREE;
+
+--
+-- Indexes for table `tb_categorias`
+--
+ALTER TABLE `tb_categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_comandas`
+--
+ALTER TABLE `tb_comandas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tb_comandas_tb_usuarios_1` (`id_usuario`) USING BTREE,
+  ADD KEY `fk_tb_comandas_tb_mesas_1` (`id_mesa`) USING BTREE;
+
+--
+-- Indexes for table `tb_comandas_produtos`
+--
+ALTER TABLE `tb_comandas_produtos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tb_comandas_produtos_tb_produtos_1` (`id_produto`) USING BTREE,
+  ADD KEY `fk_tb_comandas_produtos_tb_comandas_1` (`id_comanda`) USING BTREE;
+
+--
+-- Indexes for table `tb_mesas`
+--
+ALTER TABLE `tb_mesas`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `fk_tb_mesas_tb_usuarios_1` (`id_usuario`) USING BTREE;
+
+--
+-- Indexes for table `tb_movimento_caixa`
+--
+ALTER TABLE `tb_movimento_caixa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tb_movimento_caixa_tb_caixa_1` (`id_caixa`) USING BTREE;
+
+--
+-- Indexes for table `tb_produtos`
+--
+ALTER TABLE `tb_produtos`
+  ADD PRIMARY KEY (`id`,`cod`) USING BTREE,
+  ADD KEY `fk_tb_produtos_tb_usuarios_1` (`id_usuario`) USING BTREE,
+  ADD KEY `id_categoria` (`id_categoria`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_caixa`
+--
+ALTER TABLE `tb_caixa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+
+--
+-- AUTO_INCREMENT for table `tb_categorias`
+--
+ALTER TABLE `tb_categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tb_comandas`
+--
+ALTER TABLE `tb_comandas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tb_comandas_produtos`
+--
+ALTER TABLE `tb_comandas_produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `tb_movimento_caixa`
+--
+ALTER TABLE `tb_movimento_caixa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_produtos`
+--
+ALTER TABLE `tb_produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `tb_usuarios`
+--
+ALTER TABLE `tb_usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `tb_caixa`
+--
+ALTER TABLE `tb_caixa`
+  ADD CONSTRAINT `tb_caixa_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuarios` (`id`);
+
+--
+-- Limitadores para a tabela `tb_comandas`
+--
+ALTER TABLE `tb_comandas`
+  ADD CONSTRAINT `fk_tb_comandas_tb_usuarios_1` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuarios` (`id`),
+  ADD CONSTRAINT `tb_comandas_ibfk_1` FOREIGN KEY (`id_mesa`) REFERENCES `tb_mesas` (`id`);
+
+--
+-- Limitadores para a tabela `tb_comandas_produtos`
+--
+ALTER TABLE `tb_comandas_produtos`
+  ADD CONSTRAINT `fk_tb_comandas_produtos_tb_comandas_1` FOREIGN KEY (`id_comanda`) REFERENCES `tb_comandas` (`id`),
+  ADD CONSTRAINT `fk_tb_comandas_produtos_tb_produtos_1` FOREIGN KEY (`id_produto`) REFERENCES `tb_produtos` (`id`);
+
+--
+-- Limitadores para a tabela `tb_mesas`
+--
+ALTER TABLE `tb_mesas`
+  ADD CONSTRAINT `fk_tb_mesas_tb_usuarios_1` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuarios` (`id`);
+
+--
+-- Limitadores para a tabela `tb_movimento_caixa`
+--
+ALTER TABLE `tb_movimento_caixa`
+  ADD CONSTRAINT `fk_tb_movimento_caixa_tb_caixa_1` FOREIGN KEY (`id_caixa`) REFERENCES `tb_caixa` (`id`);
+
+--
+-- Limitadores para a tabela `tb_produtos`
+--
+ALTER TABLE `tb_produtos`
+  ADD CONSTRAINT `tb_produtos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuarios` (`id`),
+  ADD CONSTRAINT `tb_produtos_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `tb_categorias` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
