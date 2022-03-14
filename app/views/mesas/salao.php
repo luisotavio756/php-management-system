00e5d2aee95ea8b25f517f20aac768bb60ddf4de
Luis Otavio <luis.otavio2425@gmail.com>
2022-03-14 18:54:40 -0300

    Remove employee access to remove orders

--- app/views/mesas/salao.php
+++ app/views/mesas/salao.php
@@ -573,7 +573,7 @@
 									<td style="width: 60%">' + valor.descricao + '</td>\
 									<td style="width: 15%"><input type="number" min="1" max="'+(parseInt(valor.quantidade) + parseInt(valor.estoque))+'" class="form-control form-control-sm" onchange="pedidoAlt(this.value , ' + valor.id_pedido + ',' + valor.valor +')" value="' + valor.quantidade + '" required=""></td>\
 									<td style="width: 25%" class="text-center">R$ ' + valor.valor + '</td>\
-									<td style="width: 15%"><a href="#" class="text-danger" id="' + chave + '" onclick="removePedido(' + valor.id_pedido + ',' + valor.valor + ')" style="border-radius: 25px;" title="Remover Produto"><i class="fa fa-minus"></i></a></td>\
+									<td style="width: 15%; <?php if ($_SESSION['nivel'] != 1): ?> display: none <?php endif; ?>"><a href="#" class="text-danger" id="' + chave + '" onclick="removePedido(' + valor.id_pedido + ',' + valor.valor + ')" style="border-radius: 25px;" title="Remover Produto"><i class="fa fa-minus"></i></a></td>\
 								</tr>');
             			inputsHidden.append('<input type="hidden" name="pedidosAlter[' + valor.id_pedido + ']" value="' + valor.quantidade + '">');
 
